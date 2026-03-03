0cfa41a308760d07ae6817f571c7d992f414d6c4
Konstantinos K <raistlindragonl@gmail.com>
2026-03-03 23:03:33 +0200

    Update Hyprland and Waybar configurations for enhanced keyboard layout support and styling improvements

--- waybar/scripts/refresh.sh
+++ waybar/scripts/refresh.sh
@@ -4,8 +4,7 @@
 if pgrep -x "waybar" > /dev/null; then
     # If running, kill the waybar process
     pkill -x "waybar"
-else
-    # If not running, start waybar
-    waybar &
+    sleep 0.1 # Wait for a moment to ensure the process has been killed
 fi
-
+# start waybar
+waybar &
